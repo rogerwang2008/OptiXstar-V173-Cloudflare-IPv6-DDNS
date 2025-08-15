1b30acd3dafb62a9fe2bfd4421d0464ef285e952
rogerwang2008 <rogerwang2008@163.com>
2025-08-15 18:35:58 +0800

    优化，未联网时正常退出

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from rich import logging as rich_logging
 import asyncio
 import dotenv
 import os
+import requests.exceptions
 
 from ddns_utils import router_automation_async, cloudflare, ip as ip_utils
 
@@ -17,14 +18,20 @@ cloudflare.TOKEN = os.getenv("CLOUDFLARE_API_TOKEN")
 
 
 async def main():
-    ipv6 = ip_utils.get_ip_if_changed(6, True)
+    try:
+        ipv6 = ip_utils.get_ip_if_changed(6, True)
+    except requests.exceptions.ConnectionError:
+        print("Not connected to the internet!")
+        return
     if ipv6 is None:
         return
     try:
         for record_id in os.getenv("CLOUDFLARE_IPV6_RECORD_IDS").split(","):
             cloudflare.update_record_ip(os.getenv("CLOUDFLARE_ZONE_ID"), record_id, ipv6)
 
-        async with router_automation_async.RouterAutomationRunnerAsync(headless=True, screenshot=True, playwright_channel=os.getenv("PLAYWRIGHT_CHANNEL")) as runner:
+        async with router_automation_async.RouterAutomationRunnerAsync(headless=True, screenshot=True,
+                                                                       playwright_channel=os.getenv(
+                                                                               "PLAYWRIGHT_CHANNEL")) as runner:
             # 执行登录
             await runner.login()
             # 导航到端口映射页面
