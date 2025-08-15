083a9779afc9973a8bf59d45205c587547123f69
rogerwang2008 <rogerwang2008@163.com>
2025-08-15 17:39:08 +0800

    env中可以修改playwright channel

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ async def main():
     for record_id in os.getenv("CLOUDFLARE_IPV6_RECORD_IDS").split(","):
         cloudflare.update_record_ip(os.getenv("CLOUDFLARE_ZONE_ID"), record_id, ipv6)
 
-    async with router_automation_async.RouterAutomationRunnerAsync(headless=True, screenshot=True) as runner:
+    async with router_automation_async.RouterAutomationRunnerAsync(headless=True, screenshot=True, playwright_channel=os.getenv("PLAYWRIGHT_CHANNEL")) as runner:
         # 执行登录
         await runner.login()
         # 导航到端口映射页面
